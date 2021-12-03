1b4e69f267c8b4acd2599fcce7d2625d90fff6c5
cmdctrlesc <alanxyz@gmail.com>
2021-12-03 03:18:01 +0100

    accomodate new api

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 from blogapp.service import get_all_blog_titles
 from djangomockingbird.make_mocks import make_mocks
+from djangomockingbird.decorator import mock_model
+
 from blogapp.models import BlogPost
 import blogapp
 
@@ -8,7 +10,9 @@ import blogapp
 # Create your tests here.
 
 class ServiceTestCase(SimpleTestCase):
+    
 
+    @mock_model('blogapp.service.BlogPost', specs={'title':'hello'})
     def test_get_all_blog_titles(self):
         blogapp.service.BlogPost = make_mocks(BlogPost, specs={'title':'hello'})
         blog_titles = get_all_blog_titles()
