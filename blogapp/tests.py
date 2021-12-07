75acbaca0cbe83b2922fbbdd99678f6c3bc9cef8
cmdctrlesc <alanxyz@gmail.com>
2021-12-07 21:28:29 +0100

    rm unused line

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -13,6 +13,5 @@ class ServiceTestCase(SimpleTestCase):
 
     @mock_model('blogapp.service.BlogPost', specs={'title':'hello'})
     def test_get_all_blog_titles(self):
-        blogapp.service.BlogPost = make_mocks(BlogPost, specs={'title':'hello'})
         blog_titles = get_all_blog_titles()
         self.assertEqual(blog_titles, ['hello'])
